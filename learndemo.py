e8c6b267e8d826487378b9ab32e7fcd9e7b8a824
ph <1343529244@qq.com>
2024-09-19 14:10:11 +0800

    add a little

--- learndemo.py
+++ learndemo.py
@@ -9,12 +9,21 @@ def check_and_install_webdriver():
     # 检查是否已经安装了 Chrome WebDriver
     service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service,options=options)
-
-    # driver.get('http://www.whxunw.com/exam/login.thtml')
-    driver.get('http://www.baidu.com')
+    driver.get('http://www.whxunw.com/exam/login.thtml')
     driver.maximize_window()
     driver.implicitly_wait(3)
-    driver.find_element('xpath','//*[@id="kw"]').send_keys('你好')
-    driver.find_element('xpath','//*[@id="su"]').click()
-    time.sleep(10)
+    while True:
+        try:
+            driver.find_elements('xpath','//*[@id="app"]/div/div[2]/div[2]/div[2]/div[3]/div')
+            #
+            choose = driver.find_elements('xpath','//*[@id="app"]/div/div[2]/div[2]/div[2]/div[3]/div/div[2]/div/div/div[2]/div[1]/div[1]')
+            choose.click()
+            time.sleep(1)
+            be_sure = driver.find_elements('xpath','//*[@id="app"]/div/div[2]/div[2]/div[2]/div[3]/div/div[3]/button')
+            be_sure.click()
+            time.sleep(1)
+            close = driver.find_elements('xpath','//*[@id="app"]/div/div[2]/div[2]/div[2]/div[3]/div/div[3]/button')
+            close.click()
+        except:
+            time.sleep(10)
 check_and_install_webdriver()
